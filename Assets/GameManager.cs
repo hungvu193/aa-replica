ed8c53de7f1e2e3bb11fbfda7db49719e01a6dbb
hungvu <visaocuoitroi193@gmail.com>
2020-08-06 16:42:35 +0700

    initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool isGameEnded = false;
+    public Spawner spawnPoint;
+    public Rotator rotator;
+    public Animator animator;
+
+    public void EndGame()
+    {
+        if (isGameEnded == false)
+        {
+            animator.SetTrigger("EndGame");
+            isGameEnded = true;
+            spawnPoint.enabled = false;
+            rotator.enabled = false;
+            return;
+        }
+    }
+
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Pin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    public bool isPin = false;
+    public float pinSpeed = 20f;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isPin)
+            rb.MovePosition(rb.position + Vector2.up * pinSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Rotator")
+        {
+            // transform follows parents
+            transform.SetParent(collision.transform);
+            isPin = true;
+        } else if(collision.tag == "Pin")
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
